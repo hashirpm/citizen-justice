c0af9c47627fa4f6d0c61a277bd2594cb1bc4537
Hashir <mail2hashirpm@gmail.com>
2024-11-17 06:54:38 +0700

    feat: push done

--- lib/push.ts
+++ lib/push.ts
@@ -7,8 +7,9 @@ import { ethers } from "ethers";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import fs from "fs";
 
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const GEMINI_API_KEY = "AIzaSyCmHmyTnhofdOwI6vnoiS8wXyBWBez4mQU";
+const PRIVATE_KEY =
+  "584799f49952962b0022fff8ada7e36d319b0578b6809f2ffcf540b227673cf5";
 if (!GEMINI_API_KEY || !PRIVATE_KEY) {
   throw new Error(
     "Required environment variables GEMINI_API_KEY or PRIVATE_KEY are missing."
@@ -25,22 +26,55 @@ function createGenAIInstance(): GoogleGenerativeAI {
   return new GoogleGenerativeAI(GEMINI_API_KEY);
 }
 
-// Function to test summarization
-export async function createNotificationContent(
-  content: string
-): Promise<void> {
+// new event is submitted notifications
+async function createContentOnCreateEvent(
+  eventDescription: string
+): Promise<string | undefined> {
   try {
     const genAI = createGenAIInstance();
     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-    const prompt = content;
+    const prompt =
+      eventDescription +
+      "\n create an notification message on this Dispute event where people submit evidence according to the possibilities, in here we need split the content as title and description and title should be less than 120 charcters and conetnet should be lessthan 400 charcters where The title should be prefixed with title: and the description should be prefixed with description: ";
     const result = await model.generateContent(prompt);
     console.log(result.response?.text());
+
+    return result.response.text();
   } catch (error) {
     await new Promise((resolve) => setTimeout(resolve, 60000));
-    createNotificationContent(content);
+    createContentOnCreateEvent(eventDescription);
   }
 }
-createNotificationContent("what is the capital of india");
+
+const parseNotification = (inputString: string) => {
+  const titleMatch = inputString.match(/title:\s*(.*)/i);
+  const descriptionMatch = inputString.match(/description:\s*(.*)/i);
+
+  const title = titleMatch ? titleMatch[1].replace(/\*\*/g, "").trim() : null;
+  const description = descriptionMatch
+    ? descriptionMatch[1].replace(/\*\*/g, "").trim()
+    : null;
+
+  return { title, description };
+};
+
+// // Function to test summarization
+// export async function createNotificationContent(
+//   content: string
+// ): Promise<void> {
+//   try {
+//     const genAI = createGenAIInstance();
+//     const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+//     const prompt = content;
+//     const result = await model.generateContent(prompt);
+//     console.log(result.response?.text());
+//   } catch (error) {
+//     await new Promise((resolve) => setTimeout(resolve, 60000));
+//     createNotificationContent(content);
+//   }
+// }
+
+// createNotificationContent("what is the capital of india");
 
 // initialise provider
 const provider = new ethers.providers.JsonRpcProvider(
@@ -50,20 +84,29 @@ const provider = new ethers.providers.JsonRpcProvider(
 const signer = new ethers.Wallet(PRIVATE_KEY, provider);
 
 // create an function send an sample notifications
-async function sendNotification() {
+export async function sendEventCreatedNotification(eventDescription: string) {
+  var input = await createContentOnCreateEvent(eventDescription);
+
+  const { title, description } = parseNotification(input ?? "");
   // initialise push api
   const userAliace = await PushAPI.initialize(signer, {
     env: CONSTANTS.ENV.STAGING,
   });
-  const apiResponse = await userAliace.channel.send(["*"], {
-    notification: {
-      title: "Hello",
-      body: "Hello from Push Protocol",
-    },
-  });
+
+  if (title || description) {
+    const apiResponse = await userAliace.channel.send(["*"], {
+      notification: {
+        title: title ?? "New Event is created",
+        body: description ?? "Event Description",
+      },
+    });
+    console.log(apiResponse);
+  } else {
+    console.log(title);
+    console.log(description);
+  }
 
   // log the response
-  console.log(apiResponse);
 }
 
 // // call the function
