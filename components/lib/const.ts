be590c735fc445a8dba8357fd66997d352e54fd4
shiyasmohd <shiyasjaseena124@gmail.com>
2024-11-17 00:56:15 +0700

    refactor: update subgraph endpoint

--- components/lib/const.ts
+++ components/lib/const.ts
@@ -1,4 +1,4 @@
 export const CONTRACT_ADDRESS = '0x0b6d46A8387de89859E39141E17f68277698F126'
 export const NILLION_APP_BASE = "https://nillion-storage-apis-v0.onrender.com";
 export const NILLION_APP_ID = "3033c371-9335-47a1-b40a-067b78abc114";
-export const GRAPH_QUERY_URL = "http://localhost:8000/subgraphs/name/citizen-justice"
+export const GRAPH_QUERY_URL = "https://e71a-210-1-49-171.ngrok-free.app/subgraphs/name/citizen-justice"
