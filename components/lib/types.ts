4d499d4a76c1fe53c43f2b5b849735d9a6d45f52
Hashir <mail2hashirpm@gmail.com>
2024-11-17 03:21:47 +0700

    stored images display

--- components/lib/types.ts
+++ components/lib/types.ts
@@ -1,35 +1,36 @@
 export type User = {
-    acceptedSubmissions: string
-    id: string
-    isVerified: boolean
-    reputationPoints: string
-    totalSubmissions: string
-    attestationId: string | null
-    txHash: string
-    createdEvents: Event[]
-    evidences: Evidence[]
-}
+  acceptedSubmissions: string;
+  id: string;
+  isVerified: boolean;
+  reputationPoints: string;
+  totalSubmissions: string;
+  attestationId: string | null;
+  txHash: string;
+  createdEvents: Event[];
+  evidences: Evidence[];
+};
 
 export type Event = {
-    description: string
-    id: string
-    isActive: string
-    location: string
-    timestamp: string
-    txHash: string
-    categoryIds: Category[]
-}
+  description: string;
+  id: string;
+  isActive: string;
+  location: string;
+  timestamp: string;
+  txHash: string;
+  categoryIds: Category[];
+  evidences: Evidence[];
+};
 
 export type Evidence = {
-    evidenceHash: string
-    geoLocation: string
-    hasValidated: string
-    id: string
-    timestamp: string
-    txHash: string
-}
+  evidenceHash: string;
+  geoLocation: string;
+  hasValidated: string;
+  id: string;
+  timestamp: string;
+  txHash: string;
+};
 
 export type Category = {
-    id: string
-    name: string
-}
+  id: string;
+  name: string;
+};
